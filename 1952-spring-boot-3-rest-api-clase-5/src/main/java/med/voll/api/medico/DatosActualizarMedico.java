a7909b85c53806b73f497358101cebb3d0565a55
Mauricio Cardona <maucaralar@gmail.com>
2023-09-22 11:49:10 -0400

    fixed gitignore

--- 1952-spring-boot-3-rest-api-clase-5/src/main/java/med/voll/api/medico/DatosActualizarMedico.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.medico;
-
-import jakarta.validation.constraints.NotNull;
-import med.voll.api.direccion.DatosDireccion;
-
-public record DatosActualizarMedico(@NotNull Long id, String nombre, String documento, DatosDireccion direccion) {
-}
--- 1952-spring-boot-3-rest-api-clase-5/src/main/java/med/voll/api/medico/Especialidad.java
+++
@@ -1,8 +0,0 @@
-package med.voll.api.medico;
-
-public enum Especialidad {
-    ORTOPEDIA,
-    CARDIOLOGIA,
-    GINECOLOGIA,
-    PEDIATRIA
-}
